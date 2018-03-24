6578b616d84fe6728094e7dc65c342871c697b25
John Betancur <johnnyazee@gmail.com>
2018-03-23 20:41:08 -0400

    update README

--- src/themes/default.js
+++ src/themes/default.js
@@ -28,10 +28,6 @@ export default {
     lastCellPadding: '24px',
     cellPadding: '48px',
   },
-  pagination: {
-    height: '64px',
-    fontColor: 'rgba(0,0,0,.54)',
-  },
   expander: {
     backgroundColor: 'transparent',
     collapsedButton: 'data:image/svg+xml,%3Csvg%20fill%3D%22%23757575%22%20height%3D%2224%22%20viewBox%3D%220%200%2024%2024%22%20width%3D%2224%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%3E%0A%20%20%20%20%3Cpath%20d%3D%22M8.59%2016.34l4.58-4.59-4.58-4.59L10%205.75l6%206-6%206z%22/%3E%0A%20%20%20%20%3Cpath%20d%3D%22M0-.25h24v24H0z%22%20fill%3D%22none%22/%3E%0A%3C/svg%3E',
