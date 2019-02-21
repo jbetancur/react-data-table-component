7570f6f250cbe16bea8653fedd0e90f54b122ad4
John Betancur <johnnyazee@gmail.com>
2019-02-21 15:10:46 -0500

    bump version

--- src/DataTable/__tests__/TableSubheader.test..js
+++ src/DataTable/__tests__/TableSubheader.test..js
@@ -6,37 +6,37 @@ import TableSubheader from '../TableSubheader';
 
 afterEach(cleanup);
 
-test('<TableSubheader defaults should render correctly', () => {
+test('TableSubheader defaults should render correctly', () => {
   const { container } = renderWithTheme(<TableSubheader />);
 
   expect(container.firstChild).toMatchSnapshot();
 });
 
-test('<TableSubheader with component should render correctly', () => {
+test('TableSubheader with component should render correctly', () => {
   const { container } = renderWithTheme(<TableSubheader component={<div />} />);
 
   expect(container.firstChild).toMatchSnapshot();
 });
 
-test('<TableSubheader with component should render when wrap is false', () => {
+test('TableSubheader with component should render when wrap is false', () => {
   const { container } = renderWithTheme(<TableSubheader component={<div />} wrapContent={false} />);
 
   expect(container.firstChild).toMatchSnapshot();
 });
 
-test('<TableSubheader with component should render correctly with left align', () => {
+test('TableSubheader with component should render correctly with left align', () => {
   const { container } = renderWithTheme(<TableSubheader component={<div />} align="left" />);
 
   expect(container.firstChild).toMatchSnapshot();
 });
 
-test('<TableSubheader with component should render correctly with center align', () => {
+test('TableSubheader with component should render correctly with center align', () => {
   const { container } = renderWithTheme(<TableSubheader component={<div />} align="center" />);
 
   expect(container.firstChild).toMatchSnapshot();
 });
 
-test('<TableSubheader with component should render correctly with right align', () => {
+test('TableSubheader with component should render correctly with right align', () => {
   const { container } = renderWithTheme(<TableSubheader component={<div />} align="right" />);
 
   expect(container.firstChild).toMatchSnapshot();
