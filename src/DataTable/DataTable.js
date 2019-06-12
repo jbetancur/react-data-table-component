1f494bbff5ea078ae495418c127ddfcf683d972d
Lucien Knechtli <lknechtli@gmail.com>
2019-06-12 15:08:15 -0400

    Add defaultPage prop to DataTable for setting the initial page number

--- src/DataTable/DataTable.js
+++ src/DataTable/DataTable.js
@@ -52,7 +52,7 @@ class DataTable extends Component {
       sortColumn: props.defaultSortField,
       sortDirection,
       clearSelectedRows: false,
-      currentPage: 1,
+      currentPage: props.defaultPage,
       rowsPerPage: props.paginationPerPage,
     };
   }
