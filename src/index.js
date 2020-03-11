57907557cd40467a8e4471e165b55e47056bd876
justiceappiah-cult <justice.appiah@cultofcoders.com>
2020-03-11 12:51:01 +0000

    expose pagination file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import DataTable from './DataTable/DataTable';
+import Pagination from './DataTable/Pagination';
 import { createTheme, defaultThemes } from './DataTable/themes';
 
-export { defaultThemes, createTheme };
+export { defaultThemes, createTheme, Pagination };
 
 export default DataTable;
