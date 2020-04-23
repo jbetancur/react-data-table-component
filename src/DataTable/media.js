fa425fa9d456c98f03c04cb6a672d927d8695f1b
“John <johnnyazee@gmail.com>
2020-04-22 21:22:24 -0400

    fixes #500 | add responsive paging

--- src/DataTable/media.js
+++ src/DataTable/media.js
@@ -1,18 +1,28 @@
 import { css } from 'styled-components';
 
+export const XSMALL = 320;
+export const SMALL = 599;
+export const MEDIUM = 959;
+export const LARGE = 1280;
+
 export const media = {
+  xs: (...args) => css`
+  @media screen and (max-width: ${XSMALL}px) {
+    ${css(...args)}
+  }
+`,
   sm: (...args) => css`
-    @media screen and (max-width: 599px) {
+    @media screen and (max-width: ${SMALL}px) {
       ${css(...args)}
     }
   `,
   md: (...args) => css`
-    @media screen and (max-width: 959px) {
+    @media screen and (max-width: ${MEDIUM}px) {
       ${css(...args)}
     }
   `,
   lg: (...args) => css`
-    @media screen and (max-width: 1280px) {
+    @media screen and (max-width: ${LARGE}px) {
       ${css(...args)}
     }
   `,
