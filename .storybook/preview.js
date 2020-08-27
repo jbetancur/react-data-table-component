940e14516586c6ed1a8c3f9cf79da6f267a80a89
Jim Feather <jim@theindustrialresolution.com>
2020-08-27 14:54:01 -0400

    - add @storybook/addon-a11y to dev dependencies, - use paginationComponentOptions to provide the aria-label value for 'Rows Per Page' - Adjust .storybook configuration so that the a11y accessibility check functions properly   - rename config.js to preview.js (https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#to-mainjs-configuration)   - added main.js as defined in the addon-a11y package readme (https://www.npmjs.com/package/@storybook/addon-a11y)

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,5 @@
 import { configure, addDecorator } from '@storybook/react';
 import { withConsole } from '@storybook/addon-console';
-import { withA11y } from '@storybook/addon-a11y';
 import './base.css';
 
 const req = require.context('../stories', true, /\.stories\.(js|jsx)$/);
@@ -13,8 +12,14 @@ function loadStories() {
 
 const optionsCallback = (options) => ({ panelExclude: [...options.panelExclude, /Warning/] });
 
-addDecorator(withA11y);
-
 addDecorator((storyFn, context) => withConsole(optionsCallback)(storyFn)(context));
 
 configure(loadStories, module);
+export const parameters = {
+  a11y: {
+    element: '#root',
+    config: {},
+    options: {},
+    manual: true,
+  },
+};
--- src/DataTable/Pagination.js
+++ src/DataTable/Pagination.js
@@ -118,7 +118,7 @@ const Pagination = ({
   }
 
   const select = (
-    <Select onChange={handleRowsPerPage} defaultValue={rowsPerPage} aria-label="Rows Per Page">
+    <Select onChange={handleRowsPerPage} defaultValue={rowsPerPage} aria-label={paginationComponentOptions.rowsPerPageText}>
       {selectOptions}
     </Select>
   );
